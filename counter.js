0965758ae35b36de191a494f3ce447f4acdfc213
matheus78263 <78263@student.dorset-college.ie>
2026-03-09 11:59:43 +0000

    got this far with the project working

--- counter.js
+++ counter.js
@@ -1,22 +1,22 @@
-const boxCounter = document.getElementById('boxCounter');
-const bntContainer = document.getElementById('container-button');
+        const boxCounter = document.getElementById('boxCounter');
+        const btnContainer = document.getElementById('container-button');
 
-bntContainer.addEventListener('click', e => {
-    const buttonId = e.target.id;
-    let conterValue = boxCounter.innerText;
-    if (buttonId === 'bnt-decrement'){
-        counterValue--;
-    } else if (buttonId === 'bnt-reset'){
-        counterValue = 0;
-    } else if (buttonId === 'bnt-increment'){
-        counterValue++;
-    }
-    boxCounter.textContet = counterValue;
-    if (couterValue < 0){
-        boxCounter.style.color = 'red';
-    } else if (counterValue > 0){
-        boxCounter.style.color = 'green';
-    } else {
-        boxCounter.style.color = '#222';
-    }
-})
+        btnContainer.addEventListener('click', (e) => {
+            const buttonId = e.target.id;
+            let counterValue = boxCounter.innerText;
+            if (buttonId === 'btn-decrement') {
+                counterValue--;
+            } else if (buttonId === 'btn-increment') {
+                counterValue++;
+            } else if (buttonId === 'btn-reset') {
+                counterValue = 0;
+            }
+            boxCounter.innerText = counterValue;
+            if (counterValue < 0) {
+                boxCounter.style.color = 'red';
+            } else if (counterValue > 0) {
+                boxCounter.style.color = 'green';
+            } else {
+                boxCounter.style.color = '#222';
+            }
+        });
